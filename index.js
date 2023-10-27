99547783d05b2915c7ce2ac16cacaedc55a93126
Nadezhda Strelnikova <nvs.strelnikova@gmail.com>
2023-10-28 00:44:34 +0300

    game

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-import express from "express";
-const app = express();
+// import express from "express";
+// const app = express();
 
-app.use(express.static("public"));
+// app.use(express.static("public"));
 
-const server = app.listen(5500, () => {
-  const port = server.address().port;
-  console.log(`App started at http://localhost:${port}`);
-});
+// const server = app.listen(5500, () => {
+//   const port = server.address().port;
+//   console.log(`App started at http://localhost:${port}`);
+// });
